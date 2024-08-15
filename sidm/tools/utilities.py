65d24625a253e886278be4ab40706f413536d911
btcardwell <bryan.cardwell@gmail.com>
2024-08-15 19:59:04 +0000

    Fix lxy function

--- sidm/tools/utilities.py
+++ sidm/tools/utilities.py
@@ -84,7 +84,7 @@ def matched(obj1, obj2, r):
 
 def lxy(obj):
     """Return transverse distance between production and decay vertices"""
-    return (obj.dauvtx - obj.vtx).r
+    return (obj - ak.firsts(obj.children, axis=2)).r
 
 def rho(obj, ref=None):
     """Return transverse distance between object and reference (default reference is 0,0)"""
